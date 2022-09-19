b0f9a52671796b2046fba67f75a6e1cd7490142e
Melandr <melnychenko.a@gmail.com>
2022-09-19 23:35:33 +0300

    add rubber height

--- js/script.js
+++ js/script.js
@@ -5,42 +5,20 @@ const grid = document.createElement("section");
 
 const scoreSpan = document.querySelector("#atemptNumOutput");
 const resetBtn = document.querySelector("#startGame");
+const sliders = document.querySelectorAll("input");
+const columnSlider = document.querySelector("#columns");
+const rowSlider = document.querySelector("#rows");
 
-//Расширяем массив карт этим же массивом карт
-let gameGrid = [...cardsArray, ...cardsArray];
 // let gameGrid = cardsArray.concat(cardsArray);
 let firstCard, secondCard;
 let count = 0;
 let score = 0;
 let previousTarget = null;
 let delay = 1200;
+let columns = columnSlider.value,
+  rows = rowSlider.value;
 
-//Перемешиваем массив карт случайным образом
-gameGrid.sort(() => 0.5 - Math.random());
-
-grid.setAttribute("class", "grid");
-// Добавляем секцию grid в div "game"
-game.appendChild(grid);
-
-// Для каждого item в массиве карт...
-gameGrid.forEach((item) => {
-  const card = document.createElement("div");
-  card.classList.add("card");
-  card.dataset.name = item.name;
-
-  const front = document.createElement("div");
-  front.classList.add("front");
-
-  const back = document.createElement("div");
-  back.classList.add("back");
-  back.dataset.name = item.name;
-  back.style.backgroundImage = `url(${item.img})`;
-
-  // Добавляем DIV  в сетку section
-  grid.appendChild(card);
-  card.appendChild(front);
-  card.appendChild(back);
-});
+createCards(columns, rows);
 
 const match = () => {
   const selected = document.querySelectorAll(".flip");
@@ -108,5 +86,50 @@ function updateScore(amount) {
   scoreSpan.textContent = score;
 }
 
+function createCards(col, row) {
+  grid.innerHTML = "";
+
+  const size = (col * row) / 2;
+
+  //Расширяем массив карт этим же массивом карт
+  const gameGrid = [...cardsArray.slice(0, size), ...cardsArray.slice(0, size)];
+
+  //Перемешиваем массив карт случайным образом
+  gameGrid.sort(() => 0.5 - Math.random());
+
+  grid.setAttribute("class", "grid");
+  // Добавляем секцию grid в div "game"
+  game.appendChild(grid);
+
+  // Для каждого item в массиве карт...
+  gameGrid.forEach((item) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.dataset.name = item.name;
+
+    const front = document.createElement("div");
+    front.classList.add("front");
+
+    const back = document.createElement("div");
+    back.classList.add("back");
+    back.dataset.name = item.name;
+    back.style.backgroundImage = `url(${item.img})`;
+
+    // Добавляем DIV  в сетку section
+    grid.appendChild(card);
+    card.appendChild(front);
+    card.appendChild(back);
+  });
+}
+
 grid.addEventListener("click", flipCard);
 resetBtn.addEventListener("click", restart);
+
+sliders.forEach((element) => {
+  element.addEventListener("change", () => {
+    if (element.name === "columns") columns = element.value;
+    if (element.name === "rows") rows = element.value;
+
+    createCards(columns, rows);
+  });
+});
